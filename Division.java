f54f2aabd70b4dd831a456d7e446286b83a07041
Sneha-12345-stack <snehamaslekar12345@gmail.com>
2026-02-02 12:00:28 +0530

    Added Division.java by UIT2023843

---
+++ Division.java
@@ -0,0 +1,14 @@
+public class Division {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        // Checking if b is zero to avoid an error
+        if (b != 0) {
+            int result = a / b;
+            System.out.println("The result of division is: " + result);
+        } else {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+    }
+}
